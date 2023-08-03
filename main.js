af40932e6bf7b78480efb45d0a46080b9a4423b9
Soham Tonape <135266863+sohamtonape@users.noreply.github.com>
2023-08-04 01:05:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+const { app, BrowserWindow } = require("electron");
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+    mainWindow.loadFile("index.html");
+}
+app.whenReady().then(createWindow);
